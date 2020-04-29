b69be0dc1a69f62de652301406cf6ba17c972fea
study <>
2020-04-29 22:08:52 +0900

    いろいろ追加

--- var/www/app/resources/views/article_list.blade.php
+++ var/www/app/resources/views/article_list.blade.php
@@ -1,100 +1,261 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
+@section('title', 'みんなの記事一覧')
 
-        <title>Laravel</title>
+@section('stylesheet')
+  <!-- 固有css -->
+@endsection
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@section('content')
+@include('layouts.header')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+    <!--sidebar start-->
+    <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+          <p class="centered"><a href="profile.html"><img src="img/ui-sam.jpg" class="img-circle" width="80"></a></p>
+          <h5 class="centered">Sam Soffes</h5>
+          <li class="mt">
+            <a href="index.html">
+              <i class="fa fa-dashboard"></i>
+              <span>Dashboard</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-desktop"></i>
+              <span>UI Elements</span>
+              </a>
+            <ul class="sub">
+              <li><a href="general.html">General</a></li>
+              <li><a href="buttons.html">Buttons</a></li>
+              <li><a href="panels.html">Panels</a></li>
+              <li><a href="font_awesome.html">Font Awesome</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-cogs"></i>
+              <span>Components</span>
+              </a>
+            <ul class="sub">
+              <li><a href="grids.html">Grids</a></li>
+              <li><a href="calendar.html">Calendar</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+              <li><a href="todo_list.html">Todo List</a></li>
+              <li><a href="dropzone.html">Dropzone File Upload</a></li>
+              <li><a href="inline_editor.html">Inline Editor</a></li>
+              <li><a href="file_upload.html">Multiple File Upload</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-book"></i>
+              <span>Extra Pages</span>
+              </a>
+            <ul class="sub">
+              <li><a href="blank.html">Blank Page</a></li>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="lock_screen.html">Lock Screen</a></li>
+              <li><a href="profile.html">Profile</a></li>
+              <li><a href="invoice.html">Invoice</a></li>
+              <li><a href="pricing_table.html">Pricing Table</a></li>
+              <li><a href="faq.html">FAQ</a></li>
+              <li><a href="404.html">404 Error</a></li>
+              <li><a href="500.html">500 Error</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-tasks"></i>
+              <span>Forms</span>
+              </a>
+            <ul class="sub">
+              <li><a href="form_component.html">Form Components</a></li>
+              <li><a href="advanced_form_components.html">Advanced Components</a></li>
+              <li><a href="form_validation.html">Form Validation</a></li>
+              <li><a href="contactform.html">Contact Form</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-th"></i>
+              <span>Data Tables</span>
+              </a>
+            <ul class="sub">
+              <li><a href="basic_table.html">Basic Table</a></li>
+              <li><a href="responsive_table.html">Responsive Table</a></li>
+              <li><a href="advanced_table.html">Advanced Table</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="inbox.html">
+              <i class="fa fa-envelope"></i>
+              <span>Mail </span>
+              <span class="label label-theme pull-right mail-info">2</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class=" fa fa-bar-chart-o"></i>
+              <span>Charts</span>
+              </a>
+            <ul class="sub">
+              <li><a href="morris.html">Morris</a></li>
+              <li><a href="chartjs.html">Chartjs</a></li>
+              <li><a href="flot_chart.html">Flot Charts</a></li>
+              <li><a href="xchart.html">xChart</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a class="active" href="javascript:;">
+              <i class="fa fa-comments-o"></i>
+              <span>Chat Room</span>
+              </a>
+            <ul class="sub">
+              <li><a href="lobby.html">Lobby</a></li>
+              <li class="active"><a href="chat_room.html"> Chat Room</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="google_maps.html">
+              <i class="fa fa-map-marker"></i>
+              <span>Google Maps </span>
+              </a>
+          </li>
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
+    <!--sidebar end-->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Article List
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <!--main content start-->
+    <section id="main-content">
+      <section class="wrapper site-min-height">
+        <!-- page start-->
+        <div class="chat-room mt">
+          <aside class="mid-side">
+            <div class="chat-room-head">
+              <h3>Chat Room: Support</h3>
+              <form action="#" class="pull-right position">
+                <input type="text" placeholder="Search" class="form-control search-btn ">
+              </form>
             </div>
+            <div class="group-rom">
+              <div class="first-part odd">Sam Soffes</div>
+              <div class="second-part">Hi Mark, have a minute?</div>
+              <div class="third-part">12:30</div>
+            </div>
+            <div class="group-rom">
+              <div class="first-part">Mark Simmons</div>
+              <div class="second-part">Of course Sam, what you need?</div>
+              <div class="third-part">12:31</div>
+            </div>
+            <div class="group-rom">
+              <div class="first-part odd">Sam Soffes</div>
+              <div class="second-part">I want you examine the new product</div>
+              <div class="third-part">12:32</div>
+            </div>
+            <div class="group-rom">
+              <div class="first-part">Mark Simmons</div>
+              <div class="second-part">Ok, send me the pic</div>
+              <div class="third-part">12:32</div>
+            </div>
+            <div class="group-rom">
+              <div class="first-part odd">Sam Soffes</div>
+              <div class="second-part">
+                <a href="#">product.jpg</a> <span class="text-muted">35.4KB</span>
+                <p><img class="img-responsive" src="img/product.jpg" alt=""></p>
+              </div>
+              <div class="third-part">12:32</div>
+            </div>
+            <div class="group-rom">
+              <div class="first-part">Mark Simmons</div>
+              <div class="second-part">Fantastic job, love it :)</div>
+              <div class="third-part">12:32</div>
+            </div>
+            <div class="group-rom last-group">
+              <div class="first-part odd">Sam Soffes</div>
+              <div class="second-part">Thanks!!</div>
+              <div class="third-part">12:33</div>
+            </div>
+            <footer>
+              <div class="chat-txt">
+                <input type="text" class="form-control">
+              </div>
+              <div class="btn-group hidden-sm hidden-xs">
+                <button type="button" class="btn btn-white"><i class="fa fa-meh-o"></i></button>
+                <button type="button" class="btn btn-white"><i class=" fa fa-paperclip"></i></button>
+              </div>
+              <button class="btn btn-theme">Send</button>
+            </footer>
+          </aside>
+          <aside class="right-side">
+            <div class="user-head">
+              <a href="#" class="chat-tools btn-theme"><i class="fa fa-cog"></i> </a>
+              <a href="#" class="chat-tools btn-theme03"><i class="fa fa-key"></i> </a>
+            </div>
+            <div class="invite-row">
+              <h4 class="pull-left">Team Members</h4>
+              <a href="#" class="btn btn-theme04 pull-right">+ Invite</a>
+            </div>
+            <ul class="chat-available-user">
+              <li>
+                <a href="chat_room.html">
+                  <img class="img-circle" src="img/friends/fr-02.jpg" width="32">
+                  Paul Brown
+                  <span class="text-muted">1h:02m</span>
+                  </a>
+              </li>
+              <li>
+                <a href="chat_room.html">
+                  <img class="img-circle" src="img/friends/fr-05.jpg" width="32">
+                  David Duncan
+                  <span class="text-muted">1h:08m</span>
+                  </a>
+              </li>
+              <li>
+                <a href="chat_room.html">
+                  <img class="img-circle" src="img/friends/fr-07.jpg" width="32">
+                  Laura Smith
+                  <span class="text-muted">1h:10m</span>
+                  </a>
+              </li>
+              <li>
+                <a href="chat_room.html">
+                  <img class="img-circle" src="img/friends/fr-08.jpg" width="32">
+                  Julia Schultz
+                  <span class="text-muted">3h:00m</span>
+                  </a>
+              </li>
+              <li>
+                <a href="chat_room.html">
+                  <img class="img-circle" src="img/friends/fr-01.jpg" width="32">
+                  Frank Arias
+                  <span class="text-muted">4h:22m</span>
+                  </a>
+              </li>
+            </ul>
+          </aside>
         </div>
-    </body>
-</html>
+        <!-- page end-->
+      </section>
+      <!-- /wrapper -->
+    </section>
+
+    <!--footer start-->
+    <footer class="site-footer">
+      <div class="text-center">
+        <p>
+          &copy; Copyrights <strong>Dashio</strong>. All Rights Reserved
+        </p>
+        <div class="credits">
+          Created with Dashio template by <a href="https://templatemag.com/">TemplateMag</a>
+        </div>
+        <a href="chat_room.html#" class="go-top">
+          <i class="fa fa-angle-up"></i>
+          </a>
+      </div>
+    </footer>
+@endsection
---
+++ var/www/app/resources/views/layouts/master.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keyword" content="">
+    <title>@yield('title')</title>
+    <!-- Favicons -->
+    <link href="{{ asset('img/framework/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/framework/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Common CSS -->
+    <link href="{{ asset('css/framework/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/framework/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/dashio/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashio/style-responsive.css') }}" rel="stylesheet">
+    <!-- 固有 CSS -->
+    @yield('stylesheet')
+</head>
+<body>
+  <!-- =======================================================
+    contents start
+  ======================================================= -->
+    <section id="container">
+        <!-- common header -->
+        @include('layouts.header')
+        <!-- common sidebar -->
+        @include('layouts.sidebar')
+        <!-- main contents -->
+        @yield('content')
+        <!-- common footer -->
+        @include('layouts.footer')
+    </section>
+  <!-- =======================================================
+    contents end
+  ======================================================= -->
+
+
+    <!-- Common JS-->
+    <script src="{{ asset('/js/framework/jquery.min.js') }}"></script>
+    <script src="{{ asset('/js/framework/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/js/framework/jquery.dcjqaccordion.2.7.js') }}"></script>
+    <script src="{{ asset('/js/framework/jquery.scrollTo.min.js') }}"></script>
+    <script src="{{ asset('/js/framework/jquery.nicescroll.js') }}"></script>
+    <script src="{{ asset('/js/framework/common-scripts.js') }}"></script>
+</body>
+</html>

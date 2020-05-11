57a4c6b93ecd721b17670257c73676bb04a043fc
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2020-05-11 11:20:00 +0900

    記事投稿・表示作成

--- var/www/app/app/Http/Controllers/ArticleController.php
+++ var/www/app/app/Http/Controllers/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -9,7 +11,9 @@ class ArticleController extends Controller
     // みんなの記事一覧画面
     function articleList()
     {
-        return view('article_list');
+        $articles = Article::all()->sortByDesc('created_at');
+
+        return view('article_list', ['articles' => $articles]);
     }
     //みんなの記事詳細
     function articleDetails()
@@ -17,9 +21,21 @@ class ArticleController extends Controller
         return view('article_details');
     }
 
-    //マイ記事登録画面
+    //マイ記事管理画面
     function articleRegister()
     {
         return view('article_register');
+    }
+
+    //マイ記事登録処理
+    function articleStore(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->book_title = $request->book_title;
+        $article->date = $request->date;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('article.list');
     }
 }
--- var/www/app/resources/views/article_list.blade.php
+++ var/www/app/resources/views/article_list.blade.php
@@ -2,34 +2,32 @@
 @section('title', 'みんなの記事一覧')
 
 @section('stylesheet')
-  <!-- 固有css -->
+<!-- 固有css -->
 @endsection
 @section('main-content')
 <section id="main-content">
     <section class="wrapper site-min-height">
-      <div class="chat-room mt">
-        <aside class="mid-side">
-          <div class="chat-room-head">
-            <h3>コメント欄</h3>
-          </div>
-          <div class="group-rom">
-            <div class="first-part odd">テスト太郎</div>
-            <div class="second-part">コメントコメントココメントコメントコメントコメントコメント</div>
-            <div class="third-part">10:09</div>
-          </div>
-          <div class="group-rom last-group">
-            <div class="first-part odd">テスト太郎</div>
-            <div class="second-part">コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント</div>
-            <div class="third-part">10:09</div>
-          </div>
-          <footer>
-            <div class="chat-txt">
-              <input type="text" class="form-control" placeholder="コメントを入力してください">
+        <div class="container">
+            <div class="list-group mt">
+                @foreach ($articles as $article)
+                <div class="list-group-main mt">
+                    <i class="fa fa-user-circle fa-3x mr-1"></i>
+                    <div class="font-weight-bold">
+                        <a href="" class="user-name">{{ $article->user->name }}</a>
+                    </div>
+                    <a data-toggle="modal" href="#modalArticleEdit" class="list-group-item list-group-item flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h3 class="mb-1">{{ $article->title}}</h3>
+                            <p class="text-muted">{{ $article->created_at->format('Y/m/d H:i')}}</p>
+                            <h4 class="mb-1">{{ $article->book_title}}</h4>
+                            <div class="text">
+                                {!! nl2br(e( $article->body )) !!}
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                @endforeach
             </div>
-            <button class="btn btn-theme">送信</button>
-          </footer>
-        </aside>
-      </div>
     </section>
 </section>
 @endsection

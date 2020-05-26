f1870bcba90979185539fb62a44e5b8f2cda922a
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2020-05-26 16:00:51 +0900

    パスワードリセット画面修正

--- var/www/app/resources/views/auth/login.blade.php
+++ var/www/app/resources/views/auth/login.blade.php
@@ -102,41 +102,6 @@
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">パスワードをお忘れですか?</h4>
-            </div>
-            <div class="modal-body">
-                @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-                @endif
-
-                <form method="POST" action="{{ route('password.email') }}">
-                    @csrf
-
-                    <p>パスワードをリセットするには、メールアドレスを入力してください。</p>
-                    <input type="text" name="email" placeholder="メールアドレス" autocomplete="email" value="{{ old('email') }}" class="form-control placeholder-no-fix">
-
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
-
-                    <div class="modal-footer">
-                        <button data-dismiss="modal" class="btn btn-default" type="button">キャンセル</button>
-                        <button class="btn btn-theme" type="submit">送信</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- modal -->
 
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="{{ asset('/js/framework/jquery.min.js') }}"></script>
--- var/www/app/resources/views/auth/passwords/email.blade.php
+++ var/www/app/resources/views/auth/passwords/email.blade.php
@@ -10,7 +10,6 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     <h4 class="modal-title">パスワードをお忘れですか?</h4>
                 </div>
                 <div class="modal-body">
@@ -33,7 +32,6 @@
                         @enderror
 
                         <div class="modal-footer">
-                            <button data-dismiss="modal" class="btn btn-default" type="button">キャンセル</button>
                             <button class="btn btn-theme" type="submit">送信</button>
                         </div>
                     </form>

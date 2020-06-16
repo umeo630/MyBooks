de455bff07dbc361fd6f9121df6abc421339663d
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2020-06-16 21:21:38 +0900

    デザイン修正

--- var/www/app/resources/js/components/ArticleFavorite.vue
+++ var/www/app/resources/js/components/ArticleFavorite.vue
@@ -1,70 +1,63 @@
 <template>
-    <div class="centered">
-        <a
-            class="btn btn-lg btn-theme02"
-            v-if="isFavoritedBy === false"
-            @click="clickFavorite"
-        >
-            <i class="fa fa-star"> お気に入り : {{ countFavorites }}</i>
-        </a>
-        <a
-            class="btn btn-lg btn-primary"
-            v-if="isFavoritedBy === true"
-            @click="clickFavorite"
-            >お気に入り済 : {{ countFavorites }}</a
-        >
-    </div>
+  <div class="centered">
+    <a class="btn btn-theme02" v-if="isFavoritedBy === false" @click="clickFavorite">
+      <i class="fa fa-star">お気に入り : {{ countFavorites }}</i>
+    </a>
+    <a
+      class="btn btn-primary"
+      v-if="isFavoritedBy === true"
+      @click="clickFavorite"
+    >お気に入り済 : {{ countFavorites }}</a>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        initialIsFavoritedBy: {
-            type: Boolean,
-            default: false
-        },
-        initialCountFavorites: {
-            type: Number,
-            default: 0
-        },
-        authorized: {
-            type: Boolean,
-            default: false
-        },
-        endpoint: {
-            type: String
-        }
+  props: {
+    initialIsFavoritedBy: {
+      type: Boolean,
+      default: false
     },
-    data() {
-        return {
-            isFavoritedBy: this.initialIsFavoritedBy,
-            countFavorites: this.initialCountFavorites
-        };
+    initialCountFavorites: {
+      type: Number,
+      default: 0
     },
-    //クリックしたユーザーがログインしているかどうか
-    //未ログインであればアラート表示
-    methods: {
-        clickFavorite() {
-            if (!this.authorized) {
-                alert("お気に入り機能はログイン中のみ使用できます");
-                return;
-            } //お気に入り済かどうか
-            this.isFavoritedBy
-                ? this.articleUnfavorite()
-                : this.articleFavorite();
-        }, //非同期通信でお気に入りとお気に入り解除を行う
-        async articleFavorite() {
-            const response = await axios.put(this.endpoint);
-
-            this.isFavoritedBy = true;
-            this.countFavorites = response.data.countFavorites;
-        },
-        async articleUnfavorite() {
-            const response = await axios.delete(this.endpoint);
-
-            this.isFavoritedBy = false;
-            this.countFavorites = response.data.countFavorites;
-        }
+    authorized: {
+      type: Boolean,
+      default: false
+    },
+    endpoint: {
+      type: String
     }
+  },
+  data() {
+    return {
+      isFavoritedBy: this.initialIsFavoritedBy,
+      countFavorites: this.initialCountFavorites
+    };
+  },
+  //クリックしたユーザーがログインしているかどうか
+  //未ログインであればアラート表示
+  methods: {
+    clickFavorite() {
+      if (!this.authorized) {
+        alert("お気に入り機能はログイン中のみ使用できます");
+        return;
+      } //お気に入り済かどうか
+      this.isFavoritedBy ? this.articleUnfavorite() : this.articleFavorite();
+    }, //非同期通信でお気に入りとお気に入り解除を行う
+    async articleFavorite() {
+      const response = await axios.put(this.endpoint);
+
+      this.isFavoritedBy = true;
+      this.countFavorites = response.data.countFavorites;
+    },
+    async articleUnfavorite() {
+      const response = await axios.delete(this.endpoint);
+
+      this.isFavoritedBy = false;
+      this.countFavorites = response.data.countFavorites;
+    }
+  }
 };
 </script>
--- var/www/app/resources/views/article_register.blade.php
+++ var/www/app/resources/views/article_register.blade.php
@@ -128,7 +128,7 @@
                         </div>
                     </div>
                     <div class="modal" id="modal-delete-{{$article->id}}" tabindex="-1" role="dialog" aria-hidden="true">
-                        <div class="modal-dialog　modal-dialog-centerd" 　 role="document">
+                        <div class="modal-dialog　modal-dialog-centerd" role="document">
                             <div class="modal-content2">
                                 <div class="modal-header">
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">

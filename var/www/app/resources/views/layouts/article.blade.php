73c3df250e25c7c28ec0f2d40ccc6e7c705314a5
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2020-09-05 16:12:34 +0900

    Revert "コメント追加"
    This reverts commit 863b4fc2e67330f171a004256168deec95542714.

--- var/www/app/resources/views/layouts/article.blade.php
+++ var/www/app/resources/views/layouts/article.blade.php
@@ -1,3 +1,22 @@
+<!--@foreach ($articles as $article)
+<div class="list-group-main mt col-lg-4 col-md-4 col-sm-4">
+    <a href="{{ route('user.show',[$article->user->name])}}"><i class="fa fa-user-circle fa-3x mr-1"></i></a>
+    <div class="font-weight-bold">
+        <a href="{{ route('user.show',[$article->user->name])}}" class="user-name">{{$article->user->name}}</a>
+    </div>
+    <a href="{{ route('article.details',[$article->id])}}" class="list-group-item flex-column align-items-start text-center">
+        <div class="d-flex justify-content-between">
+            <h4 class="mb-1">{{ $article->book_title}}</h4>
+            <p class="text-muted">{{ $article->create_at}}</p>
+            <img src="{{ $article->url ?? 'http://design-ec.com/d/e_others_50/l_e_others_501.png'}}" width="100" height="150">
+            <div class="text">
+                {!! nl2br(e(Str::limit($article->book_content, 80, ' ...'))) !!}
+            </div>
+        </div>
+    </a>
+</div>
+@endforeach
+-->
 @foreach ($articles as $article)
 <div class="col-lg-4 col-md-4 col-sm-6 pb">
     <a href="{{ route('user.show',[$article->user->name])}}"><i class="fa fa-user-circle fa-3x mr-1"></i></a>
